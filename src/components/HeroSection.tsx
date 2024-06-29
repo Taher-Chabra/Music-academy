bebb4b10dee905f45009074d8272248581337b02
Taher Chabra <redsign12345@gmail.com>
2024-06-29 18:08:07 +0530

    changes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
          </h1>
          <p className='mt-4 font-normal text-base md:text-lg text-neutral-800 dark:text-neutral-300 max-w-lg mx-auto'>
             Dive into our comprehensive music courses and transform 
-            your musical journey today. Whether you're a beginner or 
+            your musical journey today. Whether you&apos;re a beginner or 
             looking to refine your skills, join us to unlock your full 
             potential.
          </p>
